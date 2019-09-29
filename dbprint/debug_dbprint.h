c77c8d887851b6e0a5b0e2a18bbb8228fada9ddf
Brecht Van Eeckhoudt <brecht.vaneeckhoudt@hotmail.com>
2019-09-29 13:22:41 +0200

    Added licence

--- dbprint/debug_dbprint.h
+++ dbprint/debug_dbprint.h
@@ -1,12 +1,39 @@
 /***************************************************************************//**
  * @file debug_dbprint.h
- * @brief Enable or disable printing to UART.
+ * @brief Enable or disable printing to UART with dbprint.
  * @details
  *   This header file should be called in every other file where there are UART
- *   debugging statements. Depending on the value of `DEBUG_DBPRINT`,
+ *   dbprint debugging statements. Depending on the value of `DEBUG_DBPRINT`,
  *   UART statements are enabled or disabled.
- * @version 1.0
+ * @version 1.1
  * @author Brecht Van Eeckhoudt
+ *
+ * ******************************************************************************
+ *
+ * @section License
+ *
+ *   **Copyright (C) 2019 - Brecht Van Eeckhoudt**
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the **GNU General Public License** as published by
+ *   the Free Software Foundation, either **version 3** of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   *A copy of the GNU General Public License can be found in the `LICENSE`
+ *   file along with this source code.*
+ *
+ *   @n
+ *
+ *   Some methods also use code obtained from examples from [Silicon Labs' GitHub](https://github.com/SiliconLabs/peripheral_examples).
+ *   These sections are licensed under the Silabs License Agreement. See the file
+ *   "Silabs_License_Agreement.txt" for details. Before using this software for
+ *   any purpose, you must agree to the terms of that agreement.
+ *
  ******************************************************************************/
 
 
