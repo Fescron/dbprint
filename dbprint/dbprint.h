79ff6504c671f182d46200ca43fff42aa7d0583e
Brecht Van Eeckhoudt <brecht.vaneeckhoudt@hotmail.com>
2019-11-17 21:12:14 +0100

    Updated version number

--- dbprint/dbprint.h
+++ dbprint/dbprint.h
@@ -1,7 +1,7 @@
 /***************************************************************************//**
  * @file dbprint.h
  * @brief Homebrew println/printf replacement "DeBugPrint".
- * @version 6.2
+ * @version 7.0
  * @author Brecht Van Eeckhoudt
  *
  * ******************************************************************************
